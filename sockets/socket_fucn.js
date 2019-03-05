b4b0538fb7f8220e1dcae37435001e508c86f7cf
Abhinav Singh <adityasaha890@gmail.com>
2019-03-06 02:14:48 +0530

    fF

--- sockets/socket_fucn.js
+++ sockets/socket_fucn.js
@@ -6,16 +6,16 @@ const {perma_Order,temp_Order}=require('../database/db');
 
 var io;
 var connected_socket;
+
 function connection(port){
     io=socket(port);
-
     io.on('connection',(socket)=>{
         connected_socket=socket
         console.log("made connection");
         //console.log(connected_socket);
-        connected_socket.on("request",(req)=>{
-            console.log(req);
-            io.sockets.emit("request_accepted_driver",{data:"Requets is accepted"});
+        connected_socket.on("request",(data)=>{
+            console.log(data);
+            io.sockets.emit("request_accepted_driver",(data));
         })
     })
 }
