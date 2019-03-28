5bfbcf9cb71dd96568ede6d8b9ccb7cc1beef074
Abhinav Singh <adityasaha890@gmail.com>
2019-03-28 15:50:26 +0530

    G

--- sockets/socket_fucn.js
+++ sockets/socket_fucn.js
@@ -44,6 +44,10 @@ function connection(port){
                console.log("40 func"+user);
             }).catch(err=>{console.log("38 socket_fucn"+err)});
         })
+
+        connected_socket.on("driver_from_driver_driver_frontend",data=>{
+            console.log(data);
+        })
     })
 }
 
