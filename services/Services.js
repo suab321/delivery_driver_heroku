18f22568fc63864857079ea1a01d113621873147
Abhinav Singh <adityasaha890@gmail.com>
2019-04-21 21:38:48 +0530

    g

--- services/Services.js
+++ services/Services.js
@@ -73,7 +73,7 @@ router.post('/check_recevier_otp',get_token,(req,res)=>{
 
 //function when the order completes//
 const order_complete=(Order_id)=>{
-    Order.find({Order_id:req.body.Order_id},{CurrentStatus:3}).then(user=>{
+    Order.find({Order_id:Order_id},{CurrentStatus:3}).then(user=>{
         axios.post('https://floating-brushlands-52313.herokuapp.com/authentication/order_complete',{order_id:Order_id}).then(resp=>{
             if(resp.status === 200 || 304)
                 res.status(200).json({msg:"Order Succcessfully Completed",err:"0"});
