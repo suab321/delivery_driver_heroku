c13c1969683a3c6d0202e5808925aadc4c865311
Abhinav Singh <adityasaha890@gmail.com>
2019-05-16 23:18:57 +0530

    G

--- authentication/authenticate.js
+++ authentication/authenticate.js
@@ -43,7 +43,7 @@ const verfiy=(email,token,Name)=>{
         to:email,
         subject:"Activte your Stowaway Account by verifying the link",
         text:"Click the below link for verification",
-        html:'<h3> Hi '+Name+' </h3><p>Thank You and welcome to Stowaway.To activate your account please click on the link below.</p><p><a href="https://fast-reef-53121.herokuapp.com/authentication/verification/'+token+'">'+token+'</a></p><p>If you are having trouble clicking the link,copy and paste the URL above in your web browser</p><p>From,<br>Team Stowaway</p>'
+        html:'<h3> Hi '+Name+', </h3><p>Thank You and welcome to Stowaway.To activate your account please click on the link below.</p><p><a href="https://fast-reef-53121.herokuapp.com/authentication/verification/'+token+'">'+token+'</a></p><p>If you are having trouble clicking the link,copy and paste the URL above in your web browser</p><p>Thanks,<br>Team Stowaway</p>'
     }
 
     transporter.sendMail(mailoption,(err,res)=>{
