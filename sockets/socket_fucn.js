baf63f1095b0d1a65c491cf4ce7bf4a8c27e729d
Abhinav Singh <adityasaha890@gmail.com>
2019-05-18 19:44:08 +0530

    H

--- sockets/socket_fucn.js
+++ sockets/socket_fucn.js
@@ -51,7 +51,7 @@ function connection(port){
             db.Recevier_Name=data.Recevier_Name;
             db.Recevier_Email=data.Recevier_Email;
             db.Price=data.Price;
-            db.Earning=data.Price*0.2;
+            db.Earning=data.Price*0.1;
             db.Sender_Otp=sender_unique;
             db.Recevier_Otp=recevier_unique;
             db.Weight=data.Weight
