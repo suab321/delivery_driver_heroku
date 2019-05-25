4c4efa77bed6838df633bcdb86062b8dd40dc8e7
Abhinav Singh <adityasaha890@gmail.com>
2019-05-25 12:39:06 +0530

    G

--- authentication/authenticate.js
+++ authentication/authenticate.js
@@ -86,7 +86,7 @@ const check=(req,res,next)=>{
 
 
 //registering user route
-router.post('/register',check,(req,res)=>{
+router.post('/register',(req,res)=>{
     perma.findOne({Email:req.body.Email}).then(user=>{
         if(user){
         res.status(200).json({response:"1"});
