f7c96517794ee39b5a610796c82ea6ab13187d5a
Abhinav Singh <adityasaha890@gmail.com>
2019-05-26 23:27:05 +0530

    G

--- services/Services.js
+++ services/Services.js
@@ -70,7 +70,7 @@ router.post('/check_recevier_otp',get_token,(req,res)=>{
                             axios.post(`${admin_link}/payment/pay_to_driver`,{Order_id:req.body.Order_id,Account_Id:user.Account_Id},{headers:{Authorization: `Bearer ${admin_token}`}}).then(res1=>{
                                 res.status(200).json({code:"1",msg:"Order is complete"});
                             }).catch(err=>{
-                                res.status(400).json({code:"2",msg:"eror paying the driver"})})
+                                res.status(400).json({code:"1",msg:"eror paying the driver"})})
                             })
                     })
                 }
