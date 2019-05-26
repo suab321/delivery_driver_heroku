99f59bfaabb013110dd0fbd896299a486704704c
Abhinav Singh <adityasaha890@gmail.com>
2019-05-27 02:00:43 +0530

    f

--- services/Services.js
+++ services/Services.js
@@ -66,6 +66,7 @@ router.post('/check_recevier_otp',get_token,(req,res)=>{
                         console.log(generateToken(user_id));
                             const admin_token=req.token;
                             console.log("going to admin for payment");
+                            res.status(200).json({code:"1",msg:"order delivered"});
                             // perma.findById({_id:user_id}).then(user=>{
                             // axios.post(`${admin_link}/payment/pay_to_driver`,{Order_id:req.body.Order_id,Account_Id:user.Account_Id},{headers:{Authorization: `Bearer ${admin_token}`}}).then(res1=>{
                             //     res.status(200).json({code:"1",msg:""});
